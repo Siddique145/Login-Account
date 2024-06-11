5a7ba18ec8486dccc62c531b20f35e48b6474480
Siddique145 <muhammadsiddique14344@gmail.com>
2024-06-11 14:03:14 -0700

    js code updated

--- dashbord.js
+++ dashbord.js
@@ -27,43 +27,40 @@ welUser.innerText = userName;
         setTimeout(redirection,0)
     }
 
-
     function addPost(){
-        var title = document.getElementById("title")
-        var description = document.getElementById("description")
-        var posts = document.getElementById("posts")
-
+        var title = document.getElementById("title");
+        var description = document.getElementById("description");
+        var posts = document.getElementById("posts");
+    
         if(title.value.trim() && description.value.trim()){
-        posts.innerHTML += `<div class="p-4">
-        <div class="card">
-                <div class="card-header">
-                  @Notes
-                </div>
-                <div class="card-body" style="background:#57a8ff">
-                  <blockquote class="blockquote mb-0">
-                    <p>${title.value}</p>
-                    <footer class="blockquote-footer">${description.value}<cite title="Source Title"></cite></footer>
-                  </blockquote>
-                </div>
-              </div>
-              </div>`
-              
-    var notesData = {
-        title : title.value,
-        description: description.value
-        
+            posts.innerHTML += `<div class="p-4">
+            <div class="card">
+                    <div class="card-header">
+                      @Notes
+                    </div>
+                    <div class="card-body" style="background:#57a8ff">
+                      <blockquote class="blockquote mb-0">
+                        <p>${title.value}</p>
+                        <footer class="blockquote-footer">${description.value}<cite title="Source Title"></cite></footer>
+                      </blockquote>
+                    </div>
+                  </div>
+                  </div>`;
+                  
+            var notesData = {
+                title : title.value,
+                description: description.value
+            };
+    
+            localStorage.setItem("Notes Data", JSON.stringify(notesData));
+            title.value = "";
+            description.value = "";
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Please Enter Title & Description!",
+            });
+        }
     }
-
-    localStorage.setItem("Notes Data", JSON.stringify(notesData))
-              title.value = "";
-              description.value = "";
-    }else{
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Please Enter Title & Description!",
-          });
-    }
-
-}
-addPost();
+    
